fed4fc1d159d59ea43e1a7b63fe2a479a5f28d43
berkhan ozen <berkhanozen@gmail.com>
2021-08-04 01:17:00 +0300

    End of the second day Commit

--- app/src/main/java/com/selim/basicexample/Adapter.kt
+++ app/src/main/java/com/selim/basicexample/Adapter.kt
@@ -1,15 +1,17 @@
 package com.selim.basicexample
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.cardview.widget.CardView
+import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.view.*
 import kotlinx.android.synthetic.main.coffee_row.view.*
 
 class Adapter(var list:ArrayList<Coffee>):RecyclerView.Adapter<Adapter.CoffeeVH>() {
+
+    var totalPrice = 0.0
+    var total = MutableLiveData<Double>()
 
     class CoffeeVH(itemView: View):RecyclerView.ViewHolder(itemView) {
     }
@@ -20,14 +22,50 @@ class Adapter(var list:ArrayList<Coffee>):RecyclerView.Adapter<Adapter.CoffeeVH>
     }
 
     override fun onBindViewHolder(holder: CoffeeVH, position: Int) {
+
+        var price:Double = list.get(position).price.toDouble()
+
         holder.itemView.tv_coffee_name.text = list.get(position).name
         holder.itemView.img_coffee.setImageResource(list.get(position).image)
-        holder.itemView.tv_coffee_fee.text = list.get(position).fee
+        holder.itemView.tv_coffee_price.text = list.get(position).price
+        holder.itemView.btn_add_coffee.setOnClickListener {
+
+            when(holder.itemView.rg_coffee_size.checkedRadioButtonId)
+            {
+                R.id.rb_small->price
+                R.id.rb_medium->price+=2
+                R.id.rb_large->price+=4
+            }
+            if(holder.itemView.cb_cream.isChecked)
+            {
+                price+=2
+            }
+            if(holder.itemView.cb_chocolate_syrup.isChecked)
+            {
+                price+=2
+            }
+            if(holder.itemView.sw_sugar.isChecked)
+            {
+                price+=1
+            }
+
+            totalPrice += price
+            total.value = totalPrice
+
+
+            holder.itemView.cb_cream.isChecked=false
+            holder.itemView.cb_chocolate_syrup.isChecked=false
+            holder.itemView.sw_sugar.isChecked=false
+            holder.itemView.rg_coffee_size.clearCheck()
+
+        }
     }
 
     override fun getItemCount(): Int {
         return list.size
     }
+
+
 
 }
 
--- app/src/main/java/com/selim/basicexample/MainActivity.kt
+++ app/src/main/java/com/selim/basicexample/MainActivity.kt
@@ -4,11 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.*
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.coffee_row.*
 
-data class Coffee(var name:String, var image:Int, var fee:String)
+data class Coffee(var name:String, var image:Int, var price:String)
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,6 +28,8 @@ class MainActivity : AppCompatActivity() {
         var linearLayoutManager = LinearLayoutManager(this)
         recyclerView.layoutManager = linearLayoutManager
 
+        adapter.total.observe(this, { tv_total_price.text = it.toString() })
+
 
     }
 
@@ -34,11 +37,11 @@ class MainActivity : AppCompatActivity() {
     {
         var coffeeNames = resources.getStringArray(R.array.CoffeeName)
         var coffeeImages = arrayOf(R.drawable.latte, R.drawable.macchiato, R.drawable.cappucino, R.drawable.turkish_coffee)
-        var coffeeFees = resources.getStringArray(R.array.CoffeeFee)
+        var coffeePrices = resources.getStringArray(R.array.CoffeePrice)
 
         for(i in 0..coffeeImages.size-1)
         {
-            var addedCoffee = Coffee(coffeeNames[i], coffeeImages[i], coffeeFees[i])
+            var addedCoffee = Coffee(coffeeNames[i], coffeeImages[i], coffeePrices[i])
             coffeeList.add(addedCoffee)
         }
         return coffeeList

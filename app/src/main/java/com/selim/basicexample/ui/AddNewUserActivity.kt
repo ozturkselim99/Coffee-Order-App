6ada8fc30d2279e7cfd5c6af690beabb0f46af3f
berkhan ozen <berkhanozen@gmail.com>
2021-08-05 12:13:28 +0300

    User UI Commit

---
+++ app/src/main/java/com/selim/basicexample/ui/AddNewUserActivity.kt
@@ -0,0 +1,12 @@
+package com.selim.basicexample.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.selim.basicexample.R
+
+class AddNewUserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_new_user)
+    }
+}

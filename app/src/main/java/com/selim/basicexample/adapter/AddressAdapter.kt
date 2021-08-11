8dea920fe58b9b7e54a652fe4b10020280e755d6
berkhan ozen <berkhanozen@gmail.com>
2021-08-11 14:46:54 +0300

    address commit

---
+++ app/src/main/java/com/selim/basicexample/adapter/AddressAdapter.kt
@@ -0,0 +1,29 @@
+package com.selim.basicexample.adapter
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.selim.basicexample.R
+import com.selim.basicexample.model.Address
+import kotlinx.android.synthetic.main.address_row.view.*
+
+class AddressAdapter(val addressList:ArrayList<Address>) : RecyclerView.Adapter<AddressAdapter.AddressVH>() {
+    class AddressVH(itemView:View) : RecyclerView.ViewHolder(itemView){
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AddressVH {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.address_row, parent, false)
+        return AddressVH(itemView)
+    }
+
+    override fun onBindViewHolder(holder: AddressVH, position: Int) {
+        holder.itemView.tv_address_name.text = addressList.get(position).addressName
+        holder.itemView.tv_address_info.text = addressList.get(position).city + " " + addressList.get(position).district + " " +
+                addressList.get(position).avenue + " " + addressList.get(position).neighborhood + " " + addressList.get(position).street + " " +
+                addressList.get(position).buildingNumber + " " + addressList.get(position).number
+    }
+
+    override fun getItemCount() = addressList.size
+}

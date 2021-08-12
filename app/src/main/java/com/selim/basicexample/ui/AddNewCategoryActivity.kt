c98b291db621ed353fa19cfa88d0b26d7feb3a11
yasincetin <yasincetin@teknasyon.com>
2021-08-12 14:45:59 +0300

    add category from Firestore

--- app/src/main/java/com/selim/basicexample/ui/AddNewCategoryActivity.kt
+++ app/src/main/java/com/selim/basicexample/ui/AddNewCategoryActivity.kt
@@ -1,14 +1,56 @@
 package com.selim.basicexample.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.FirebaseFirestore
 import com.selim.basicexample.R
+import com.selim.basicexample.model.CoffeeCategory
 
 class AddNewCategoryActivity : AppCompatActivity() {
+    private val buttonAdd by lazy { findViewById<View>(R.id.btn_add_category_to_db) }
+    private val editTextCategoryName by lazy { findViewById<EditText>(R.id.et_category_name) }
+    private val editTextUrl by lazy { findViewById<EditText>(R.id.et_image_url) }
+
+    var firestore: FirebaseFirestore? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_new_category)
 
+        firestore = FirebaseFirestore.getInstance()
 
+        buttonAdd.setOnClickListener {
+            addNewCategory()
+        }
+    }
+
+    private fun addNewCategory() {
+        val document = firestore?.collection("category")
+
+        val category = CoffeeCategory().apply {
+            name = editTextCategoryName.text.toString()
+            imageUrl = editTextUrl.text.toString()
+        }
+
+        document?.add(category)?.addOnCompleteListener { task ->
+            when (task.isSuccessful) {
+                true -> {
+                    Toast.makeText(
+                        this,
+                        "${category.name} kategorisi Eklendi..",
+                        Toast.LENGTH_LONG
+                    ).show()
+                    finish()
+                }
+                false -> Toast.makeText(
+                    this,
+                    "${category.name} kategorisi Eklenemedi..",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
     }
 }
--- app/src/main/java/com/selim/basicexample/ui/CoffeeCategoryActivity.kt
+++ app/src/main/java/com/selim/basicexample/ui/CoffeeCategoryActivity.kt
@@ -1,30 +1,53 @@
 package com.selim.basicexample.ui
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.firestore.FirebaseFirestore
 import com.selim.basicexample.R
 import com.selim.basicexample.adapter.CategoryMenuAdapter
-import com.selim.basicexample.data.MockData
 import com.selim.basicexample.model.CoffeeCategory
 import kotlinx.android.synthetic.main.activity_coffee_category.*
 
 class CoffeeCategoryActivity : AppCompatActivity() {
+    var firestore: FirebaseFirestore? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_coffee_category)
 
+        firestore = FirebaseFirestore.getInstance()
+
         val linearLayout = LinearLayoutManager(this)
         recycler_view_category_menu.layoutManager = linearLayout
-
-        val categoryMenuAdapter = CategoryMenuAdapter(MockData.getCoffeeCategories())
-        recycler_view_category_menu.adapter = categoryMenuAdapter
 
         fab_add_category.setOnClickListener {
             val intent = Intent(this, AddNewCategoryActivity::class.java)
             startActivity(intent)
         }
 
+        getCategories()
+    }
+
+    private fun getCategories() {
+        firestore?.collection("category")?.get()?.addOnSuccessListener { snapshot ->
+            val list = arrayListOf<CoffeeCategory>()
+
+            snapshot.documents.forEach { documentSnapshot ->
+                documentSnapshot.toObject(CoffeeCategory::class.java)?.let { category ->
+                    category.id = documentSnapshot.id
+                    list.add(category)
+                }
+            }
+
+            loadCategories(list)
+
+        }
+    }
+
+    private fun loadCategories(list: ArrayList<CoffeeCategory>) {
+        val categoryMenuAdapter = CategoryMenuAdapter(list)
+        recycler_view_category_menu.adapter = categoryMenuAdapter
     }
 }
